126c107d260ca7d519a6aaeab60aca396ff7d861
Stefan <stefanxenadu@gmail.com>
2022-10-19 20:37:32 +0200

    Linked HAL with CPP Utils and Folder Restruct

---
+++ Inc/C++Utilities/CppUtils.hpp
@@ -0,0 +1,30 @@
+#pragma once
+
+#include <map>
+#include <unordered_map>
+#include <vector>
+#include <string>
+#include <sstream>
+#include <set>
+#include <tuple>
+#include <unordered_set>
+#include <utility>
+#include <chrono>
+#include <algorithm>
+#include <functional>
+#include <array>
+#include <bitset>
+#include <ctype.h>
+#include <malloc.h>
+#include <math.h>
+#include <deque>
+#include <queue>
+#include <stack>
+#include <initializer_list>
+#include <iterator>
+#include <limits>
+#include <memory>
+#include <random>
+#include <typeinfo>
+
+using namespace std;
