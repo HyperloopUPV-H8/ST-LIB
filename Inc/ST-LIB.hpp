8ad43f0ac631e050f6fe0c60b75703cc4fe98f71
Alejandro <alejandrogonhid@gmail.com>
2022-10-22 09:38:20 +0200

    working on PWM hash and equal

--- Inc/ST-LIB.hpp
+++ Inc/ST-LIB.hpp
@@ -3,3 +3,4 @@
 #include "HALAL/Models/PinModel/Pin.hpp"
 #include "HALAL/Services/DigitalOutput/DigitalOutput.hpp"
 #include "HALAL/Services/DigitalInput/DigitalInput.hpp"
+#include "HALAL/Services/PWM/PWM.hpp"
