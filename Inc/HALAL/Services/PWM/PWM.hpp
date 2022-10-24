6579558b2abc640f31dddb4c681e6a1a8207244f
Alejandro <alejandrogonhid@gmail.com>
2022-10-24 17:07:35 +0200

    delete comments

--- Inc/HALAL/Services/PWM/PWM.hpp
+++ Inc/HALAL/Services/PWM/PWM.hpp
@@ -14,9 +14,6 @@
 struct TimerChannel {
 	TIM_HandleTypeDef* timer;
 	unsigned int channel;
-
-	//TimerChannel() = default;
-	//TimerChannel(uint32_t t, unsigned int c) :timer(t),channel(c) {}
 };
 
 class PWM {
@@ -24,7 +21,6 @@ public:
 	static map<uint8_t, Pin> serviceIDs;
 	static map<Pin, TimerChannel> pinTimerMap;
 
-	//static unordered_map<Pin, TimerChannel, KeyHash> pinTimerMap;
 	static forward_list<uint8_t> IDmanager;
 
 	static optional<uint8_t> register_pwm(Pin& pin);
