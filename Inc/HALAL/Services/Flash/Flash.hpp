dd3f6864218bdb05cbe808c919924122751e6d2e
Pablo González Segarra <pablogscraft@gmail.com>
2022-10-25 09:53:15 +0200

    semifunctional version, problems with programmer

--- Inc/HALAL/Services/Flash/Flash.hpp
+++ Inc/HALAL/Services/Flash/Flash.hpp
@@ -13,7 +13,14 @@
 
 #define FLASHWORD 8
 #define FLASH_START_ADDRESS 0x08000000
+#define FLASH_SECTOR0_START_ADDRESS 0x08000000
+#define FLASH_SECTOR1_START_ADDRESS 0x08020000
+#define FLASH_SECTOR2_START_ADDRESS 0x08040000
+#define FLASH_SECTOR3_START_ADDRESS 0x08060000
 #define FLASH_SECTOR4_START_ADDRESS 0x08080000
+#define FLASH_SECTOR5_START_ADDRESS 0x080A0000
+#define FLASH_SECTOR6_START_ADDRESS 0x080C0000
+#define FLASH_SECTOR7_START_ADDRESS 0x080E0000
 #define FLASH_END_ADDRESS 0x080FFFFF
 #define SECTOR_SIZE_IN_WORDS 32767
 
@@ -34,7 +41,9 @@ class Flash{
 public:
 	static void read(uint32_t sourceAddr, uint32_t* result, uint32_t numberOfWords);
 
-	static void  write(uint32_t* source, uint32_t destAddr, uint32_t sizeInWords);
+	static bool write(uint32_t* source, uint32_t destAddr, uint32_t sizeInWords);
+
+	static bool erase(uint32_t initAddr, uint32_t finalAddr); //TODO: Este metodo tiene que ser privado antes de un pull request
 
 private:
 
@@ -42,7 +51,7 @@ private:
 
 	static uint32_t getSectorStartingAddress(uint32_t sector);
 
-	static void erase(uint32_t initAddr, uint32_t finalAddr);
+
 
 };
 
--- Src/HALAL/Services/Flash/FlashTests/Flash_Test.cpp
+++ Src/HALAL/Services/Flash/FlashTests/Flash_Test.cpp
@@ -19,7 +19,6 @@ namespace FlashTest{
 		for (uint8_t i = 0; i < 4; i++) {
 		  ftoa_bytes_temp[i] = thing.bytes[i];
 		}
-
 	}
 
 	float Bytes2float(uint8_t * ftoa_bytes_temp){
@@ -39,13 +38,16 @@ namespace FlashTest{
 	bool Test1_Writing_1_float(){
 		float pi = 3.141592;
 
+		//uint32_t addresses[] = {0x00000000, 0x00000010, 0x00000020, 0x00000028};
 		uint8_t float_byte_array[4];
-		float2Bytes(float_byte_array, pi);
-		uint32_t addresses[] = {0x00000000, 0x00000012, 0x00000020, 0x00000028};
-		uint32_t addr =FLASH_SECTOR4_START_ADDRESS + addresses[rand()% 4];
 
-		Flash::write((uint32_t *)float_byte_array, addr,(uint32_t) 1);
-		return true;
+		float2Bytes(float_byte_array, pi);
+		uint32_t addr = FLASH_SECTOR5_START_ADDRESS; // + addresses[rand()% 4];
+
+
+		return Flash::write((uint32_t *)float_byte_array, addr,(uint32_t) 1)&
+		 Flash::write((uint32_t *)float_byte_array, addr + 0x20,(uint32_t) 1)&
+		 Flash::write((uint32_t *)float_byte_array, addr + 0x40,(uint32_t) 1);
 	}
 
 
