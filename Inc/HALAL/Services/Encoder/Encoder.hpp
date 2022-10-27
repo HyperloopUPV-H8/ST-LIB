221204ce409db95c8eb40d13b0c64e4d663db8ca
Pablo González Segarra <pablogscraft@gmail.com>
2022-10-27 02:06:56 +0200

    added encoder core files

---
+++ Inc/HALAL/Services/Encoder/Encoder.hpp
@@ -0,0 +1,17 @@
+/*
+ * Encoder.hpp
+ *
+ *  Created on: 27 oct. 2022
+ *      Author: Pablo
+ */
+
+#pragma once
+
+#include "../../Models/PinModel/Pin.hpp"
+#include "../../../C++Utilities/CppUtils.hpp"
+
+
+class Encoder {
+public:
+
+};
---
+++ Src/HALAL/Services/Encoder/Encoder.cpp
@@ -0,0 +1,13 @@
+/*
+ * Encoder.cpp
+ *
+ *  Created on: 27 oct. 2022
+ *      Author: Pablo
+ */
+
+#include "../Inc/HALAL/Services/Encoder/Encoder.hpp"
+
+
+
+
+
