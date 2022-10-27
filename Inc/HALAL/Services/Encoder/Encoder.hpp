2e32df4fa024089d82e193766b61bc6b51ce61a9
Pablo González Segarra <pablogscraft@gmail.com>
2022-10-27 02:19:01 +0200

    added core methods

--- Inc/HALAL/Services/Encoder/Encoder.hpp
+++ Inc/HALAL/Services/Encoder/Encoder.hpp
@@ -13,5 +13,11 @@
 
 class Encoder {
 public:
+	static optional<uint8_t> register_encoder(Pin pin);
 
+	static void turn_on_encoder(uint8_t id);
+
+	static void turn_off_encoder(uint8_t id);
+
+	static void get_encoder_value(uint8_t id);
 };
