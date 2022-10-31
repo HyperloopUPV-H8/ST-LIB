bea354e85044eeb26b1e24cdc3e5775bf446309a
Daniel González Castiñeiras <dgoncas2906@gmail.com>
2022-10-31 09:32:02 +0100

    Initial implementation of Time module

---
+++ Inc/HALAL/Services/Time/Time.hpp
@@ -0,0 +1,64 @@
+/*
+ * Time.hpp
+ *
+ *  Created on: 30 oct. 2022
+ *      Author: Dani
+ */
+
+#pragma once
+#include "C++Utilities/CppUtils.hpp"
+#include "stm32h7xx_hal.h"
+
+// HIGH RESOLUTION TIMERS
+extern TIM_HandleTypeDef htim2;		// Used for the global timer (3,36nS step)
+
+extern TIM_HandleTypeDef htim5;		// Used for the high precision alarms (1uS)
+extern TIM_HandleTypeDef htim24;	// Used for the high precision alarms (1uS)
+
+// LOW RESOLUTION TIMERS
+extern TIM_HandleTypeDef htim6;		// Used for the low precision alarms (1mS)
+
+class Time {
+
+private :
+
+	struct Alarm {
+		uint32_t period;
+		TIM_HandleTypeDef* tim;
+		function<void()> alarm;
+	};
+
+	static const uint32_t HIGH_PRECISION_MAX_ARR = 4294967295;
+	static const uint32_t APB1_TIM_FREQ = 275000000;
+	static uint64_t global_tick;
+	static uint64_t low_precision_tick;
+
+	static uint16_t high_precision_alarm_id_count;
+	static uint16_t low_precision_alarm_id_count;
+	static map<uint8_t, Alarm> high_precision_alarms_by_id;
+	static map<TIM_HandleTypeDef*, Alarm> high_precision_alarms_by_timer;
+	static map<uint8_t, Alarm> low_precision_alarms_by_id;
+
+	static void start_timer(TIM_HandleTypeDef* handle, uint32_t prescaler, uint32_t arr);
+	static void stop_timer(TIM_HandleTypeDef* handle);
+
+public :
+	static TIM_HandleTypeDef* global_timer;
+
+	static set<TIM_HandleTypeDef*> high_precision_timers;
+	static stack<TIM_HandleTypeDef*> available_high_precision_timers;
+
+	static TIM_HandleTypeDef* low_precision_timer;
+
+	static void high_precision_timer_callback(TIM_HandleTypeDef* tim);
+	static void global_timer_callback();
+	static void low_precision_timer_callback();
+
+	static void start();
+	static uint64_t get_global_tick();
+	static optional<uint8_t> register_high_precision_alarm(function<void()> tick_func, uint32_t period_in_uS);
+	static bool unregister_high_precision_alarm(uint16_t id);
+
+	static uint8_t register_low_precision_alarm(function<void()> tick_func, uint32_t period_in_mS);
+	static bool unregister_low_precision_alarm(uint16_t id);
+};
---
+++ Src/HALAL/Services/Time/Time.cpp
@@ -0,0 +1,131 @@
+/*
+ * Time.cpp
+ *
+ *  Created on: 30 oct. 2022
+ *      Author: Dani
+ */
+
+#include <Time/Time.hpp>
+
+uint64_t Time::global_tick = 0;
+uint64_t Time::low_precision_tick = 0;
+
+TIM_HandleTypeDef* Time::global_timer = &htim2;
+set<TIM_HandleTypeDef*> Time::high_precision_timers;
+TIM_HandleTypeDef* Time::low_precision_timer = &htim6;
+
+stack<TIM_HandleTypeDef*> Time::available_high_precision_timers;
+
+uint16_t Time::high_precision_alarm_id_count = 0;
+uint16_t Time::low_precision_alarm_id_count = 0;
+map<uint8_t, Time::Alarm> Time::high_precision_alarms_by_id;
+map<uint8_t, Time::Alarm> Time::low_precision_alarms_by_id;
+map<TIM_HandleTypeDef*, Time::Alarm> Time::high_precision_alarms_by_timer;
+
+// START AND STOP METHODS
+
+void Time::start(){
+	Time::start_timer(global_timer, 0, Time::HIGH_PRECISION_MAX_ARR);
+	Time::start_timer(low_precision_timer, 275, 1000);
+
+	Time::available_high_precision_timers.push(&htim24);
+	Time::available_high_precision_timers.push(&htim5);
+
+	Time::high_precision_timers.insert(&htim24);
+	Time::high_precision_timers.insert(&htim5);
+}
+
+void Time::start_timer(TIM_HandleTypeDef* handle, uint32_t prescaler, uint32_t arr){
+	handle -> Instance -> ARR = arr;
+	handle -> Instance -> PSC = prescaler;
+	HAL_TIM_Base_Start_IT(handle);
+}
+
+void Time::stop_timer(TIM_HandleTypeDef* handle){
+	HAL_TIM_Base_Stop_IT(handle);
+}
+
+// PUBLIC SERVICE METHODS
+
+uint64_t Time::get_global_tick(){
+	uint64_t current_tick = Time::global_tick + global_timer->Instance->CNT;
+	double to_nanoseconds = 1.0 / Time::APB1_TIM_FREQ * 1000000000.0;
+	return current_tick * to_nanoseconds;
+}
+
+
+optional<uint8_t> Time::register_high_precision_alarm(function<void()> alarm_func, uint32_t period_in_uS){
+	if(available_high_precision_timers.size() == 0)
+		return {};
+
+	uint16_t id = Time::high_precision_alarm_id_count++;
+	TIM_HandleTypeDef* tim = Time::available_high_precision_timers.top();
+	Time::available_high_precision_timers.pop();
+
+	Time::Alarm alarm = { period_in_uS, tim, alarm_func };
+	Time::high_precision_alarms_by_id[id] = alarm;
+	Time::high_precision_alarms_by_timer[tim] = alarm;
+
+	Time::start_timer(tim, 275, period_in_uS);
+
+	return {id};
+}
+
+bool Time::unregister_high_precision_alarm(uint16_t id){
+	if(Time::high_precision_alarms_by_id.find(id) == high_precision_alarms_by_id.end())
+		return false;
+
+	Time::Alarm alarm = high_precision_alarms_by_id[id];
+	Time::available_high_precision_timers.push(alarm.tim);
+	Time::stop_timer(alarm.tim);
+
+	Time::high_precision_alarms_by_id.erase(id);
+	Time::high_precision_alarms_by_timer.erase(alarm.tim);
+
+	return true;
+}
+
+uint8_t Time::register_low_precision_alarm(function<void()> alarm_func, uint32_t period_in_uS){
+	uint16_t id = Time::low_precision_alarm_id_count++;
+	Time::Alarm alarm = { period_in_uS, low_precision_timer, alarm_func };
+	Time::low_precision_alarms_by_id[id] = alarm;
+	return { id };
+}
+
+bool Time::unregister_low_precision_alarm(uint16_t id){
+	if(Time::low_precision_alarms_by_id.find(id) == low_precision_alarms_by_id.end());
+		return false;
+	Time::low_precision_alarms_by_id.erase(id);
+	return true;
+}
+
+// HALAL TIMER CALLBACKS
+
+void Time::global_timer_callback(){
+	Time::global_tick += Time::HIGH_PRECISION_MAX_ARR;
+}
+
+void Time::high_precision_timer_callback(TIM_HandleTypeDef* tim){
+	Time::high_precision_alarms_by_timer[tim].alarm();
+}
+
+void Time::low_precision_timer_callback(){
+	for(auto pair : Time::low_precision_alarms_by_id){
+		Time::Alarm alarm = pair.second;
+		if(Time::low_precision_tick % alarm.period == 0)
+			alarm.alarm();
+	}
+
+	low_precision_tick += 1;
+}
+
+// HAL TIMER CALLBACK
+
+void HAL_TIM_PeriodElapsedCallback(TIM_HandleTypeDef* tim){
+	if(tim == Time::global_timer)
+			Time::global_timer_callback();
+	if(Time::high_precision_timers.count(tim) !=0)
+			Time::high_precision_timer_callback(tim);
+	if(tim == Time::low_precision_timer)
+			Time::low_precision_timer_callback();
+}
