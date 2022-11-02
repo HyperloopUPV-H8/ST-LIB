c08833abab1e4fbec65468aab76a5e7488c41b4d
Alejandro Gonzalvo <alejandrogonhid@gmail.com>
2022-11-02 11:37:02 +0100

    reestructure whole PWM and PWMN handling

--- Inc/HALAL/Services/PWM/PWM.hpp
+++ Inc/HALAL/Services/PWM/PWM.hpp
@@ -17,15 +17,23 @@ struct TimerChannel {
 class PWM {
 public:
 	static map<uint8_t, Pin> service_ids;
+	static map<uint8_t, Pin> service_ids_negated;
+	static map<uint8_t, pair<Pin, Pin>> service_ids_dual;
 	static map<Pin, TimerChannel> pin_timer_map;
+	static map<Pin, TimerChannel> pin_timer_map_negated;
+	static map<pair<Pin, Pin>, TimerChannel> pin_timer_map_dual;
 
 	static forward_list<uint8_t> id_manager;
 
 	static optional<uint8_t> register_pwm(Pin& pin);
+	static optional<uint8_t> register_pwm_negated(Pin& pin);
+	static optional<uint8_t> register_pwm_dual(Pin& pin, Pin& pin_negated);
+
 	static void unregister_pwm(uint8_t id);
+
 	static void turn_on_pwm(uint8_t id);
-	static void turn_on_pwm_negated(uint8_t id);
+
 	static void turn_off_pwm(uint8_t id);
-	static void turn_off_pwm_negated(uint8_t id);
+
 	static void change_duty_cycle(uint8_t id, uint8_t duty_cicle);
 };
