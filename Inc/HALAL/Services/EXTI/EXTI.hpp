6f479caa19989cf30e6edac28ebcce589615f042
Alejandro Gonzalvo <alejandrogonhid@gmail.com>
2022-11-06 10:27:29 +0100

    add action system and read function

--- Inc/HALAL/Services/EXTI/EXTI.hpp
+++ Inc/HALAL/Services/EXTI/EXTI.hpp
@@ -6,23 +6,28 @@
  */
 
 #pragma once
+#include <C++Utilities/CppUtils.hpp>
 #include "ST-LIB.hpp"
 
-struct GPIOValue {
+typedef function<void()> Action;
+
+
+struct EXTIdata {
 	uint16_t gpio;
-	bool value;
+	Action* action;
+	bool is_on = true;
 };
 
 class ExtI {
 public:
 	static map<uint8_t, Pin> service_ids;
-	static map<Pin, GPIOValue> pin_gpio_map;
+	static map<Pin, EXTIdata> pin_gpio_map;
 
 	static forward_list<uint8_t> id_manager;
 
-	static optional<uint8_t> register_exti(Pin& pin);
+	static optional<uint8_t> register_exti(Pin& pin, Action* action);
 	static void unregister_exti(uint8_t id);
 	static void turn_on_exti(uint8_t id);
 	static void turn_off_exti(uint8_t id);
-	bool* get_pin_value(pin);
+	bool get_pin_value(uint8_t id);
 };
