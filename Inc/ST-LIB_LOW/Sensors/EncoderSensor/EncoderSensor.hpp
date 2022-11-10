527df96aa3ecea0fe54a89fe7d923052c9558c90
ricardo chust <ricardochustfides@gmail.com>
2022-11-10 15:29:16 +0100

    encoder read half done (need timer on lastcheck)

--- Inc/ST-LIB_LOW/Sensors/EncoderSensor/EncoderSensor.hpp
+++ Inc/ST-LIB_LOW/Sensors/EncoderSensor/EncoderSensor.hpp
@@ -13,8 +13,8 @@
 
 class EncoderSensor : public Sensor<double>::Sensor{
 public:
-	EncoderSensor(Pin pin1, Pin pin2, double *value);
-	EncoderSensor(Pin pin1, Pin pin2, Pin pin3, double *value);
+	EncoderSensor(Pin pin1, Pin pin2, double *value, double counter_distance);
+	EncoderSensor(Pin pin1, Pin pin2, Pin pin3, double *value, double counter_distance);
 	void read();
 	uint8_t getID();
 
@@ -24,4 +24,6 @@ protected:
 	Pin pin3;
 	uint8_t id;
 	double* value;
+	double counter_distance;
+	uint32_t last_check;
 };
--- Src/ST-LIB_LOW/Sensors/EncoderSensor/EncoderSensor.cpp
+++ Src/ST-LIB_LOW/Sensors/EncoderSensor/EncoderSensor.cpp
@@ -3,23 +3,26 @@
 
 //I have to get the pin's address as register_encoder works with pin pointers.
 //quick note, all other services use references but ADC, which uses the value itself.
-EncoderSensor::EncoderSensor(Pin pin1, Pin pin2, double* value)
-: value(value){
+EncoderSensor::EncoderSensor(Pin pin1, Pin pin2, double* value, double counter_distance)
+: value(value), counter_distance(counter_distance){
 	optional<uint8_t> identification = Encoder::register_encoder(&pin1,&pin2);
 	if(identification){
 		id = identification.value();
 		Encoder::turn_on_encoder(id);
+		last_check = 1;
 	}else{
 		//errores de optional vacio aqui (por hacer)
 	}
 }
 
 void EncoderSensor::read(){
-	optional<long unsigned int> counter = Encoder::get_encoder_counter(id);
+	optional<uint32_t> counter = Encoder::get_encoder_counter(id);
 	optional<bool> direction = Encoder::get_encoder_direction(id);
 	if(counter && direction){
-		//calcular valores aqui (por hacer)
-		*value = (double) counter.value();
+		//calcular valores aqui [0] velocidad y [1] posicion
+		int sign = (1 - 2*direction.value());
+		value[0] = sign * ((double) counter.value()) * counter_distance / (last_check);
+		value[1] = value[1] + sign * ((double) counter.value()) * counter_distance;
 		Encoder::reset_encoder(id);
 	}
 	else{
