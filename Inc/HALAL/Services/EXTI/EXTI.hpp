a49c77dcf04acdc89988e65082d558d4af8eb6bf
Alejandro Gonzalvo <alejandrogonhid@gmail.com>
2022-11-11 17:27:48 +0100

    change exti to ExternalInterrupt

--- Inc/HALAL/Services/EXTI/EXTI.hpp
+++ Inc/HALAL/Services/EXTI/EXTI.hpp
@@ -9,23 +9,20 @@
 #include <C++Utilities/CppUtils.hpp>
 #include "ST-LIB.hpp"
 
-typedef function<void()> Action;
-
-
 struct EXTIdata {
 	uint16_t gpio;
-	Action* action = nullptr;
+	function<void()>* action = nullptr;
 	bool is_on = true;
 };
 
-class ExtI {
+class ExternalInterrupt {
 public:
 	static map<uint8_t, Pin> service_ids;
 	static map<Pin, EXTIdata> pin_gpio_map;
 
 	static forward_list<uint8_t> id_manager;
 
-	static optional<uint8_t> register_exti(Pin& pin, Action* action);
+	static optional<uint8_t> register_exti(Pin& pin, function<void()>* action);
 	static void unregister_exti(uint8_t id);
 	static void turn_on_exti(uint8_t id);
 	static void turn_off_exti(uint8_t id);
