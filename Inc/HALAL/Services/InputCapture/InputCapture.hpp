55587eb548bdda85958adb75ad3233aec3a42197
Alejandro Gonzalvo <alejandrogonhid@gmail.com>
2022-11-12 16:58:16 +0100

    erase instance_constructor data

--- Inc/HALAL/Services/InputCapture/InputCapture.hpp
+++ Inc/HALAL/Services/InputCapture/InputCapture.hpp
@@ -36,14 +36,8 @@ public:
 		Instance(Pin p, TIM_HandleTypeDef* tim, uint32_t ch_r, uint32_t ch_f);
 	};
 
-	struct instance_constructor_data {
-		TIM_HandleTypeDef* timer;
-		uint32_t channel_rising;
-		uint32_t channel_falling;
-	};
-
 	static map<uint8_t, IC::Instance> instances;
-	static map<Pin, instance_constructor_data> instances_data;
+	static map<Pin, IC::Instance> instances_data;
 	static forward_list<uint8_t> id_manager;
 
 	static optional<uint8_t> register_ic(Pin& pin);
