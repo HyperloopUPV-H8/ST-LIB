e0608ad5c50f2bc3680b9532ee4fc993e633231c
Alejandro Gonzalvo <alejandrogonhid@gmail.com>
2022-11-15 08:52:03 +0100

    added include guards for tim and adc

--- Inc/ST-LIB.hpp
+++ Inc/ST-LIB.hpp
@@ -5,6 +5,11 @@
 #include "DigitalInput/DigitalInput.hpp"
 #include "Flash/Flash.hpp"
 #include "Flash/FlashTests/Flash_Test.hpp"
+#ifdef HAL_ADC_MODULE_ENABLED
 #include "ADC/ADC.hpp"
+#endif
+
+#ifdef HAL_TIM_MODULE_ENABLED
 #include "PWM/PWM.hpp"
 #include "Encoder/Encoder.hpp"
+#endif
