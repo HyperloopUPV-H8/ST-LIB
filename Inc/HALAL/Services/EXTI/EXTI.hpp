b0fabd6811a3f0838daac22618cd74b64271d6f6
Alejandro Gonzalvo <alejandrogonhid@gmail.com>
2022-11-15 14:35:42 +0100

    change parameter name for consistency

--- Inc/HALAL/Services/EXTI/EXTI.hpp
+++ Inc/HALAL/Services/EXTI/EXTI.hpp
@@ -23,7 +23,7 @@ public:
 	static map<uint16_t, Instance> instances;
 	static forward_list<uint8_t> id_manager;
 
-	static optional<uint8_t> register_exti(Pin& _pin, function<void()>* _action);
+	static optional<uint8_t> register_exti(Pin& pin, function<void()>* action);
 	static void unregister_exti(uint8_t id);
 	static void turn_on_exti(uint8_t id);
 	static void turn_off_exti(uint8_t id);
