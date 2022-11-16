0b7d5af9e59f48744929144d5865105413b15a25
ricardo chust <ricardochustfides@gmail.com>
2022-11-16 12:28:11 +0100

    protection against underflow added

--- Src/ST-LIB_LOW/Sensors/EncoderSensor/EncoderSensorWTime.cpp
+++ Src/ST-LIB_LOW/Sensors/EncoderSensor/EncoderSensorWTime.cpp
@@ -2,9 +2,9 @@
 #include "Encoder/Encoder.hpp"
 #include "Time/Time.hpp"
 
-const int n_frames = 5; //debe ser >1
+const int n_frames = 5; //debe ser >1, cuanto mas alto mayor suavizado y mayor coste de computacion
 const double frame_size = 0.01; //sacrificar tiempo de refresco por minima velocidad detectable (mas alto, mejor precision (minimo baja), mas bajo mejor tiempo de refresco)
-uint32_t counters[n_frames];
+uint32_t counters[n_frames]; //minima velocidad detectable es igual a counter_distance * n_frames / frame_size
 uint64_t times[n_frames];
 
 EncoderSensor::EncoderSensor(Pin pin1, Pin pin2, double *position, double *speed, double *acceleration)
@@ -25,18 +25,24 @@ EncoderSensor::EncoderSensor(Pin pin1, Pin pin2, double *position, double *speed
 }
 
 void EncoderSensor::read(){
-	optional<uint32_t> counter = Encoder::get_encoder_counter(id);
-	optional<bool> direction = Encoder::get_encoder_direction(id);
+	optional<uint32_t> optional_counter = Encoder::get_encoder_counter(id);
+	optional<bool> optional_direction = Encoder::get_encoder_direction(id);
 	uint64_t time = Time::get_global_tick();
 	
-	if(counter && direction){
+	if(optional_counter && optional_direction){
+		uint32_t counter = optional_counter.value();
+
 		if(time - times[n_frames-1] >= frame_size*clock_frequency){
 			for(int i = 1; i < n_frames; i++){
 				counters[i-1] = counters[i];
 				times[i-1] = times[i];
 			}
 
-			counters[n_frames-1] = counter.value();
+			if(counter - counters[n_frames-1] > 0xEFFFFFFF){
+				counter = ~counter;
+			}
+
+			counters[n_frames-1] = counter;
 			times[n_frames-1] = time;
 			last_speed = *speed;
 		}
