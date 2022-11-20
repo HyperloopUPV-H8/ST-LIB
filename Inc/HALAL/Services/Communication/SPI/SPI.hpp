107d4b467818e230b74087816703e046840b39b1
Pablo <pablogscraft@gmail.com>
2022-11-20 07:44:56 +0100

    added  doxygen documentation

--- Inc/HALAL/Services/Communication/SPI/SPI.hpp
+++ Inc/HALAL/Services/Communication/SPI/SPI.hpp
@@ -13,18 +13,29 @@
 
 extern SPI_HandleTypeDef hspi3;
 
+
+/**
+ * @brief SPI service class. Abstracts the use of the encoder with the HAL library.SPI service for SPI communication 
+ * 
+ */
 class SPI{
 
 private:
+	/**
+	 * @brief Struct wich defines all data refering to SPI peripherals. It is 
+	 * 		  declared private in order to prevent unwanted use. Only 
+	 * 		  predefined instaces should be used. 
+	 * 		  
+	 */
 	struct Peripheral
 	{
-		Pin SCK;
-		Pin MOSI;
-		Pin MISO;
-		Pin SS;
-		SPI_HandleTypeDef* hspi;
-		optional<SPIPacket> tx_buffer;
-		bool receive_ready;
+		Pin SCK; /**< Clock pin. */  
+		Pin MOSI; /**< MOSI pin. */  
+		Pin MISO; /**< MISO pin. */  
+		Pin SS; /**< Slave select pin. */  
+		SPI_HandleTypeDef* hspi;  /**< HAL spi struct pin. */  
+		optional<SPIPacket> tx_buffer; /**< Transceive temporal buffer pin. */  
+		bool receive_ready; /**< Receive value is ready to use pin. */  
 
 		bool operator<(const SPI::Peripheral& other) const {
 			return SS < other.SS;
@@ -36,6 +47,10 @@ private:
 		}
 	};
 
+	/**
+	 * @brief Struct only used as hash operator for unoredered map.
+	 * 
+	 */
 	struct SPIPeripheral_hash_function {
 		size_t operator()(const SPI::Peripheral& spi) const
 		{
@@ -47,16 +62,63 @@ public:
 	static forward_list<uint8_t> ID_manager;
 	static unordered_map<uint8_t, SPI::Peripheral* > registered_spi;
 
+	/**
+	 * @brief SPI 3 of the STM32H723.
+	 * 
+	 */
 	static SPI::Peripheral peripheral3;
 
+	/**
+	 * @brief This method register a new SPI.
+	 * 
+	 * @param spi SPI peripheral to register.
+	 * @return optional<uint8_t> Id of the service.
+	 */
 	static optional<uint8_t> register_SPI(SPI::Peripheral& spi);
 
+	/**
+	 * @brief This method is used for transmit 1 SPIPacket of any size by DMA and
+	 * 		  interrupts. It handles the pakcet size automatically. In order to
+	 * 		  to send various packet in a row you must check if the spi is busy
+	 * 		  using the method is_busi().
+	 * 
+	 * @param id Id of the SPI
+	 * @param packet Pakcet to be send
+	 * @return bool Returns true if the request to send the packet has been done
+	 * 		   successfully. Returns false if the SPI is busy or a problem
+	 * 		   has occurred.
+	 */
 	static bool transmit_next_packet(uint8_t id, SPIPacket& packet);
 
+	/**
+	 * @brief This method request the receive of a new SPIPacket of any size
+	 * 	      by DMA and interrupts. Thus the packet should not be used until
+	 * 	  	  you have checked that the value is already available using the 
+	 * 	 	  method has_next_paclet().
+	 * @see   SPI::has_next_packet()
+	 * 
+	 * @param id Id of the SPI
+	 * @param packet SPIPacket in which the data will be stored
+	 * @return bool Return true if the order to receive a new packet has been
+	 * 		   processed correctly. Return false if the SPI is busy or a
+	 * 		   problem has occurred.
+	 */
 	static bool receive_next_packet(uint8_t id, SPIPacket& packet);
 
+	/**
+	 * @brief This method is used to check if the receive packet is ready.
+	 * 
+	 * @param id Id of the SPI
+	 * @return bool Return true if the packet is ready to use and false if not.
+	 */
 	static bool has_next_packet(uint8_t id);
 
+	/**
+	 * @brief This method is used to check if the SPI is busy.
+	 * 
+	 * @param id Id of the SPI
+	 * @return bool Return true if the SPI is busy and false if not.
+	 */
 	static bool is_busy(uint8_t id);
 
 };
