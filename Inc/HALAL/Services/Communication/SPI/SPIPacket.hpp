ef0fac964f119574af6524585971d943709f66cc
Pablo <pablogscraft@gmail.com>
2022-11-20 19:58:34 +0100

    added the documentation of the SPIPacket class

--- Inc/HALAL/Services/Communication/SPI/SPIPacket.hpp
+++ Inc/HALAL/Services/Communication/SPI/SPIPacket.hpp
@@ -7,15 +7,29 @@
 
 #pragma once
 
+/**
+ * @brief SPI data pakcet class. This class contains a pointer to the data stored
+ * 	 	  in bytes and the number of bytes.
+ * 
+ */
 class SPIPacket
 {
 	uint32_t size;
     uint8_t* data;
 
-public:
 
+private:
 	SPIPacket(){};
 
+public:
+
+	/**
+	 * @brief Construct a new SPIPacket object. The constructor will handle automaticly
+	 * 		  the data type and size automatically.
+	 * 
+	 * @tparam Type Generic data type.
+	 * @param data Data to be stored in the packet.
+	 */
 	template<typename Type>
 	SPIPacket(Type& data) {
 		this->size = sizeof(Type);
@@ -24,6 +38,14 @@ public:
 		memcpy(this->data, &data, this->size);
 	}
 
+	/**
+	 * @brief Construct a new SPIPacket object. Create a SPIPacket by passing an array
+	 * 		  of any type and length.
+	 * 
+	 * @tparam Type Generic data type.
+	 * @param data Pointer to the data to be store.
+	 * @param size Size of the array.
+	 */
 	template<typename Type>
 	SPIPacket(Type* data, uint32_t size){
 		this->size = size * sizeof(Type);
@@ -31,15 +53,29 @@ public:
 		memcpy(this->data, data, this->size);
 	}
 
+	/**
+	 * @brief Destroy the SPIPacket object.
+	 * 
+	 */
 	~SPIPacket() {
 		if (data)
 			free(data);
 	}
 
+	/**
+	 * @brief Get the data size in bytes.
+	 * 
+	 * @return uint32_t Returns the data size in bytes.
+	 */
 	uint32_t get_size() {
 		return size;
 	}
 
+	/**
+	 * @brief Get the pointer to the data.
+	 * 
+	 * @return uint8_t* Returns a pointer to the packet data.
+	 */
 	uint8_t* get_data() {
 		return data;
 	}
