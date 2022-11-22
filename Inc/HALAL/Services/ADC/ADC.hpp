a61eb15cd14fe9b989af31c6e7fae7b08cca3179
ricardo chust <ricardochustfides@gmail.com>
2022-11-22 12:01:00 +0100

    removed error at copying

--- Inc/HALAL/Services/ADC/ADC.hpp
+++ Inc/HALAL/Services/ADC/ADC.hpp
@@ -63,17 +63,3 @@ public:
 	static void turn_on(uint8_t id);
 	static optional<float> get_value(uint8_t id);
 };
-Footer
-© 2022 GitHub, Inc.
-Footer navigation
-Terms
-Privacy
-Security
-Status
-Docs
-Contact GitHub
-Pricing
-API
-Training
-Blog
-About
