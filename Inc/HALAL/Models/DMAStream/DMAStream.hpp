1372bba2456e1ccc3ab2bad6c6238984a412946c
alejandrogonzalvo <alejandrogonhid@protonmail.com>
2022-11-24 11:41:19 +0100

    [DOESN'T WORK] intent to erase adc_buf_len constants

--- Inc/HALAL/Models/DMAStream/DMAStream.hpp
+++ Inc/HALAL/Models/DMAStream/DMAStream.hpp
@@ -10,9 +10,8 @@
 struct DMAStream {
 public:
 	uint16_t* buffer;
-	uint8_t buffer_length;
 	bool is_on = false;
 
 	DMAStream() = default;
-	DMAStream(uint16_t* buffer, uint8_t buffer_length) : buffer(buffer), buffer_length(buffer_length), is_on(false) {};
+	DMAStream(uint16_t* buffer) : buffer(buffer), is_on(false) {};
 };
