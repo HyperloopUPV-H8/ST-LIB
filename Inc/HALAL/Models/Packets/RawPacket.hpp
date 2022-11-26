1899d9e39fe6a0139d05689e0430a99fa74b9bfe
Pablo <pablogscraft@gmail.com>
2022-11-26 22:41:52 +0100

    changed al tabs to spaces

--- Inc/HALAL/Models/Packets/RawPacket.hpp
+++ Inc/HALAL/Models/Packets/RawPacket.hpp
@@ -9,72 +9,72 @@
 
 /**
  * @brief Raw data packet class. Contains a pointer to the data stored
- * 	 	  in bytes and the number of bytes.
+ *            in bytes and the number of bytes.
  *
  */
 class RawPacket
 {
-	uint32_t size; /**< Size in bytes of the data. */
+    uint32_t size; /**< Size in bytes of the data. */
     uint8_t* data; /**< Data pointer. */
 
 private:
     RawPacket(){};
 
 public:
-	/**
-	 * @brief Constructs a new RawPacket object. The constructor will handle automatically
-	 * 		  the data type and size.
-	 *
-	 * @tparam Type Generic data type.
-	 * @param data Data to be stored in the packet.
-	 */
-	template<typename Type>
-	RawPacket(Type* data) {
-		this->size = sizeof(Type);
-		this->data = (uint8_t*)malloc(this->size);
+    /**
+     * @brief Constructs a new RawPacket object. The constructor will handle automatically
+     *           the data type and size.
+     *
+     * @tparam Type Generic data type.
+     * @param data Data to be stored in the packet.
+     */
+    template<typename Type>
+    RawPacket(Type* data) {
+        this->size = sizeof(Type);
+        this->data = (uint8_t*)malloc(this->size);
 
-		memcpy(this->data, data, this->size);
-	}
+        memcpy(this->data, data, this->size);
+    }
 
-	/**
-	 * @brief Constructs a new RawPacket object by passing an array
-	 * 		  of any type and length.
-	 *
-	 * @tparam Type Generic data type.
-	 * @param data Pointer to the data to be store.
-	 * @param size Size of the array.
-	 */
-	template<typename Type>
-	RawPacket(Type* data, uint32_t size){
-		this->size = size * sizeof(Type);
-		this->data = (uint8_t*)malloc(this->size);
-		memcpy(this->data, data, this->size);
-	}
+    /**
+     * @brief Constructs a new RawPacket object by passing an array
+     *           of any type and length.
+     *
+     * @tparam Type Generic data type.
+     * @param data Pointer to the data to be store.
+     * @param size Size of the array.
+     */
+    template<typename Type>
+    RawPacket(Type* data, uint32_t size){
+        this->size = size * sizeof(Type);
+        this->data = (uint8_t*)malloc(this->size);
+        memcpy(this->data, data, this->size);
+    }
 
-	/**
-	 * @brief Deletes a RawPacket instance.
-	 *
-	 */
-	~RawPacket() {
-		if (data)
-			free(data);
-	}
+    /**
+     * @brief Deletes a RawPacket instance.
+     *
+     */
+    ~RawPacket() {
+        if (data)
+            free(data);
+    }
 
-	/**
-	 * @brief Gets the data size in bytes.
-	 *
-	 * @return uint32_t Returns the data size in bytes.
-	 */
-	uint32_t get_size() {
-		return size;
-	}
+    /**
+     * @brief Gets the data size in bytes.
+     *
+     * @return uint32_t Returns the data size in bytes.
+     */
+    uint32_t get_size() {
+        return size;
+    }
 
-	/**
-	 * @brief Get the pointer to the data.
-	 *
-	 * @return uint8_t* Returns a pointer to the packet data.
-	 */
-	uint8_t* get_data() {
-		return data;
-	}
+    /**
+     * @brief Get the pointer to the data.
+     *
+     * @return uint8_t* Returns a pointer to the packet data.
+     */
+    uint8_t* get_data() {
+        return data;
+    }
 };
