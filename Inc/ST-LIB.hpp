7b874b17ed3a146236341311fda662078d7565c1
alejandrogonzalvo <59616818+alejandrogonzalvo@users.noreply.github.com>
2022-11-27 19:42:39 +0100

    Update ST-LIB.hpp

--- Inc/ST-LIB.hpp
+++ Inc/ST-LIB.hpp
@@ -1,4 +1,4 @@
 #pragma once
 
-// Highest layer up to the moment
-#include "ST-LIB_LOW.hpp"
+#include "HALAL/HALAL.hpp" // Erase this when ST-LIB_LOW is finished.
+#include "ST-LIB_LOW.hpp" // Highest layer up to the moment
