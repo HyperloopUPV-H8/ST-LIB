654c1981135fea94bbb638eb46386c2b6a93bea6
alejandrogonzalvo <59616818+alejandrogonzalvo@users.noreply.github.com>
2022-11-27 19:44:36 +0100

    Update HALAL.hpp

--- Inc/HALAL/HALAL.hpp
+++ Inc/HALAL/HALAL.hpp
@@ -7,8 +7,6 @@
 #include "ADC/ADC.hpp"
 #include "PWM/PWM.hpp"
 #include "Time/Time.hpp"
-#include "Clocks/Counter.hpp"
-#include "Clocks/Stopwatch.hpp"
 #include "InputCapture/InputCapture.hpp"
 #include "Encoder/Encoder.hpp"
 #include "EXTI/EXTI.hpp"
