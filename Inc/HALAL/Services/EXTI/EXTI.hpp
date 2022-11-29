54517835229d0333c18b21202f65c37f0c999455
alejandrogonzalvo <alejandrogonhid@protonmail.com>
2022-11-29 11:48:14 +0100

    add EXTI Start

--- Inc/HALAL/Services/EXTI/EXTI.hpp
+++ Inc/HALAL/Services/EXTI/EXTI.hpp
@@ -15,8 +15,12 @@ class ExternalInterrupt {
 public:
 	class Instance {
 	public:
+		IRQn_Type interrupt_request_number;
 		function<void()> action = nullptr;
 		bool is_on = true;
+
+		Instance() = default;
+		Instance(IRQn_Type interrupt_request_number);
 	};
 
 	static map<uint8_t, Pin> service_ids;
@@ -24,6 +28,7 @@ public:
 	static forward_list<uint8_t> id_manager;
 
 	static optional<uint8_t> inscribe(Pin& pin, function<void()>&& action);
+	static void start();
 	static void turn_on(uint8_t id);
 	static void turn_off(uint8_t id);
 	static optional<bool> get_pin_value(uint8_t id);
