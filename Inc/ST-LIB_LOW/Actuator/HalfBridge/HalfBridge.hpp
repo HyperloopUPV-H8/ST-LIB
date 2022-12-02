f00404fe7883cb1614e825c1e6ec6a9669208306
alejandrogonzalvo <alejandrogonhid@protonmail.com>
2022-12-02 20:45:28 +0100

    erase redundant deadtime

--- Inc/ST-LIB_LOW/Actuator/HalfBridge/HalfBridge.hpp
+++ Inc/ST-LIB_LOW/Actuator/HalfBridge/HalfBridge.hpp
@@ -14,16 +14,15 @@
 class HalfBridge {
 public:
 	HalfBridge() = default;
-	HalfBridge(Pin& positive_voltage_pwm_pin, Pin& negative_voltage_pwm_pin, Pin& enable_pin, uint8_t deadtime_in_clock_cycles = 45);
+	HalfBridge(Pin& positive_voltage_pwm_pin, Pin& negative_voltage_pwm_pin, Pin& enable_pin);
 	HalfBridge(Pin& positive_voltage_pwm_pin, Pin& positive_voltage_pwm_negated_pin, Pin& negative_voltage_pwm_pin, Pin& negative_voltage_pwm_negated_pin,
-			Pin& enable_pin, uint8_t deadtime_in_clock_cycles = 45);
+			Pin& enable_pin);
 
 	void turn_on();
 	void turn_off();
 	void set_duty_cycle(int8_t duty_cycle);
 
 private:
-	uint8_t deadtime_in_clock_cycles;
 	PWM positive_voltage_pwm;
 	PWM negative_voltage_pwm;
 	DigitalOutput enable;
