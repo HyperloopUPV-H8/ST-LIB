8bb649ab10593b923d98cce40a8c88a14ae095b1
ricardo chust <ricardochustfides@gmail.com>
2022-12-06 16:05:32 +0100

    arctg struct (TODO)

--- Inc/HALAL/Services/CORDIC/CORDIC.hpp
+++ Inc/HALAL/Services/CORDIC/CORDIC.hpp
@@ -27,12 +27,18 @@
 
 class RotationComputer{
 public:
+	//a 180º angle is 2147483647.
 	static void cos(int32_t *angle,int32_t *out,int size);
 	static void sin(int32_t *angle,int32_t *out,int size);
 	static void cos_and_sin(int32_t *angle, int32_t *cos_out, int32_t *sin_out, int size);
-	static void phase(int32_t *x, int32_t *y, int32_t *angle_out, int size); // vector (1,0) is phase 0
-	static void modulus(int32_t *x, int32_t *y, int32_t *out, int size); //cannot give a value higher than 2147483392
+	//vector values can go from (-2147483647 to 2147483647)
+	// vector (x,0) is phase 0
+	static void phase(int32_t *x, int32_t *y, int32_t *angle_out, int size);
+	//cannot give a value higher than 2147483392
+	static void modulus(int32_t *x, int32_t *y, int32_t *out, int size);
 	static void phase_and_modulus(int32_t *x, int32_t *y, int32_t *angle_out, int32_t *mod_out, int size);
+	//max value for angle output is 89.55º (input needs to be <128)
+	static void arctg(int32_t *tg, int32_t *angle_out, int size);
 	static Operation_Computation mode;
 	static float q31_to_f32(uint32_t in);
 	static int32_t f32_to_q31(double in);
--- Src/HALAL/Services/CORDIC/CORDIC.cpp
+++ Src/HALAL/Services/CORDIC/CORDIC.cpp
@@ -101,6 +101,20 @@ void RotationComputer::phase_and_modulus(int32_t *x, int32_t *y, int32_t *angle_
 		}
 }
 
+//TODO
+void RotationComputer::arctg(int32_t *tg, int32_t *angle_out, int size){
+	if(RotationComputer::mode != ARCTANGENT){
+		RotationComputer::mode = ARCTANGENT;
+		MODIFY_REG(CORDIC -> CSR,
+					0x007F07FF,
+					0x00000054
+		);
+	}
+	for(int n = 0; n < size; n++){
+		CORDIC -> WDATA = tg[n];
+		angle_out[n] = CORDIC -> RDATA;
+	}
+}
 
 /*In CSR you can directly configurate CORDIC. The masks for the variables are defined as follow
 	 Functions : 0x0000000F (0.COSINE 1.SINE 2.PHASE 3.MODULUS 4.ARCTANGENT 5.HCOSINE 6.HSINE 7.HARCTANGENT 8.NATLOG 9.SQRT)
