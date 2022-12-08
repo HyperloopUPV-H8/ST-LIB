969e557ba14cfd02378822a29b3d5e6ffdf37f0e
alejandrogonzalvo <alejandrogonhid@protonmail.com>
2022-12-08 11:10:17 +0100

    use service instead of instance classes

--- Inc/ST-LIB_LOW/Actuator/HalfBridge/HalfBridge.hpp
+++ Inc/ST-LIB_LOW/Actuator/HalfBridge/HalfBridge.hpp
@@ -8,14 +8,14 @@
 #pragma once
 
 #include "Time/Time.hpp"
-#include "Actuator/PWM/PWM.hpp"
-#include "Actuator/DigitalOutput/DigitalOutput.hpp"
+#include "DigitalOutputservice/DigitalOutputservice.hpp"
+#include "PWMservice/PWMservice.hpp"
 
 class HalfBridge {
 public:
 	HalfBridge() = default;
-	HalfBridge(Pin& positive_voltage_pwm_pin, Pin& negative_voltage_pwm_pin, Pin& enable_pin);
-	HalfBridge(Pin& positive_voltage_pwm_pin, Pin& positive_voltage_pwm_negated_pin, Pin& negative_voltage_pwm_pin, Pin& negative_voltage_pwm_negated_pin,
+	HalfBridge(Pin& positive_pwm_pin, Pin& negative_pwm_pin, Pin& enable_pin);
+	HalfBridge(Pin& positive_pwm_pin, Pin& positive_pwm_negated_pin, Pin& negative_pwm_pin, Pin& negative_pwm_negated_pin,
 			Pin& enable_pin);
 
 	void turn_on();
@@ -23,7 +23,7 @@ public:
 	void set_duty_cycle(int8_t duty_cycle);
 
 private:
-	PWM positive_voltage_pwm;
-	PWM negative_voltage_pwm;
-	DigitalOutput enable;
+	uint8_t positive_pwm;
+	uint8_t negative_pwm;
+	uint8_t enable;
 };
--- Src/ST-LIB_LOW/Actuator/HalfBridge/HalfBridge.cpp
+++ Src/ST-LIB_LOW/Actuator/HalfBridge/HalfBridge.cpp
@@ -7,39 +7,39 @@
 
 #include "Actuator/HalfBridge/HalfBridge.hpp"
 
-HalfBridge::HalfBridge(Pin& positive_voltage_pwm_pin, Pin& negative_voltage_pwm_pin, Pin& enable_pin){
-	HalfBridge::positive_voltage_pwm = PWM(positive_voltage_pwm_pin);
-	HalfBridge::negative_voltage_pwm = PWM(negative_voltage_pwm_pin);
-	HalfBridge::enable = DigitalOutput(enable_pin);
+HalfBridge::HalfBridge(Pin& positive_pwm_pin, Pin& negative_pwm_pin, Pin& enable_pin){
+	HalfBridge::positive_pwm = PWMservice::inscribe(positive_pwm_pin);
+	HalfBridge::positive_pwm = PWMservice::inscribe(negative_pwm_pin);
+	HalfBridge::enable = DigitalOutputservice::inscribe(enable_pin);
 
 }
 
-HalfBridge::HalfBridge(Pin& positive_voltage_pwm_pin, Pin& positive_voltage_pwm_negated_pin,
-		Pin& negative_voltage_pwm_pin, Pin& negative_voltage_pwm_negated_pin, Pin& enable_pin) {
-	HalfBridge::positive_voltage_pwm = PWM(positive_voltage_pwm_pin, positive_voltage_pwm_negated_pin);
-	HalfBridge::negative_voltage_pwm = PWM(negative_voltage_pwm_pin, negative_voltage_pwm_negated_pin);
-	HalfBridge::enable = DigitalOutput(enable_pin);
+HalfBridge::HalfBridge(Pin& positive_pwm_pin, Pin& positive_pwm_negated_pin,
+		Pin& negative_pwm_pin, Pin& negative_pwm_negated_pin, Pin& enable_pin) {
+	HalfBridge::positive_pwm = PWMservice::inscribe_dual(positive_pwm_pin, positive_pwm_negated_pin);
+	HalfBridge::negative_pwm = PWMservice::inscribe_dual(negative_pwm_pin, negative_pwm_negated_pin);
+	HalfBridge::enable = DigitalOutputservice::inscribe(enable_pin);
 }
 
 void HalfBridge::turn_on() {
-	positive_voltage_pwm.turn_on();
-	negative_voltage_pwm.turn_on();
-	enable.turn_on(); // enable al final para evitar ruido
+	PWMservice::turn_on(positive_pwm);
+	PWMservice::turn_on(negative_pwm);
+	DigitalOutputservice::turn_on(enable); // enable al final para evitar ruido
 }
 
 void HalfBridge::turn_off() {
-	enable.turn_off(); // enable al principio para evitar ruido
-	positive_voltage_pwm.turn_off();
-	negative_voltage_pwm.turn_off();
+	DigitalOutputservice::turn_off(enable); // enable al principio para evitar ruido
+	PWMservice::turn_off(positive_pwm);
+	PWMservice::turn_off(negative_pwm);
 }
 
 void HalfBridge::set_duty_cycle(int8_t duty_cycle) {
 	if (duty_cycle >= 0) {
-		negative_voltage_pwm.set_duty_cycle(0);
-		positive_voltage_pwm.set_duty_cycle(duty_cycle);
+		PWMservice::set_duty_cycle(negative_pwm, 0);
+		PWMservice::set_duty_cycle(positive_pwm, duty_cycle);
 	}
 	else {
-		positive_voltage_pwm.set_duty_cycle(0);
-		negative_voltage_pwm.set_duty_cycle(duty_cycle);
+		PWMservice::set_duty_cycle(positive_pwm, 0);
+		PWMservice::set_duty_cycle(negative_pwm, duty_cycle);
 	}
 }
