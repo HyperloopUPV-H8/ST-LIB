ac53a3f7ac1d1019f8877bbce073d4650c6141ae
alejandrogonzalvo <alejandrogonhid@protonmail.com>
2022-12-13 11:57:30 +0100

    create Vehicle Class

---
+++ Inc/ST-LIB_LOW/StateMachine/Vehicle/Vehicle.hpp
@@ -0,0 +1,34 @@
+/*
+ * Vehicle.hpp
+ *
+ *  Created on: 13 dic. 2022
+ *      Author: aleja
+ */
+
+#pragma once
+
+#include "StateMachine/StateMachine.hpp"
+#include "Time/Time.hpp"
+
+class Vehicle {
+public:
+	enum States : uint8_t {
+		OFF = 0,
+		ON = 1,
+		SLEEP = 2
+	};
+
+	static void initialize();
+	static void turn_on();
+	static void turn_off();
+	static void to_sleep();
+
+	static uint8_t add_cyclic_action(uint8_t milliseconds, function<void()> action);
+	static uint8_t add_cyclic_sleep_action(uint8_t milliseconds, function<void()> action);
+	static void remove_cyclic_action(uint8_t id);
+
+	static void add_transition(States old_state, States new_state, function<bool()> transition);
+
+private:
+	static StateMachine state_machine;
+};
