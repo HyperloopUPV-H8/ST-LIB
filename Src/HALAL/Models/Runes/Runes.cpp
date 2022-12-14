bd0a9fc043637ea2cca4d4ceb4c23f11fb001482
alejandrogonzalvo <alejandrogonhid@protonmail.com>
2022-12-14 18:13:51 +0100

    update runes

--- Src/HALAL/Models/Runes/Runes.cpp
+++ Src/HALAL/Models/Runes/Runes.cpp
@@ -28,41 +28,78 @@ map<pair<Pin, Pin>, TimerPeripheral*> Encoder::pin_timer_map = {
 
 extern TIM_HandleTypeDef htim1;
 extern TIM_HandleTypeDef htim2;
+extern TIM_HandleTypeDef htim3;
+extern TIM_HandleTypeDef htim4;
+extern TIM_HandleTypeDef htim12;
+extern TIM_HandleTypeDef htim16;
+extern TIM_HandleTypeDef htim17;
 extern TIM_HandleTypeDef htim15;
 extern TIM_HandleTypeDef htim23;
 
 TimerPeripheral::InitData init_data_timer1(TIM1);
 TimerPeripheral::InitData init_data_timer2(TIM2);
+TimerPeripheral::InitData init_data_timer3(TIM3);
+TimerPeripheral::InitData init_data_timer4(TIM4);
+TimerPeripheral::InitData init_data_timer12(TIM12);
+TimerPeripheral::InitData init_data_timer16(TIM16);
+TimerPeripheral::InitData init_data_timer17(TIM17);
 TimerPeripheral::InitData init_data_timer15(TIM15);
 TimerPeripheral::InitData init_data_timer23(TIM23);
 
 TimerPeripheral timer1(&htim1, init_data_timer1);
 TimerPeripheral timer2(&htim2, init_data_timer2);
+TimerPeripheral timer3(&htim3, init_data_timer3);
+TimerPeripheral timer4(&htim4, init_data_timer4);
+TimerPeripheral timer12(&htim12, init_data_timer12);
+TimerPeripheral timer16(&htim16, init_data_timer16);
+TimerPeripheral timer17(&htim17, init_data_timer17);
 TimerPeripheral timer15(&htim15, init_data_timer15);
 TimerPeripheral timer23(&htim23, init_data_timer23);
 
 vector<reference_wrapper<TimerPeripheral>> TimerPeripheral::timers = {
 		timer1,
 		timer2,
+		timer3,
+		timer4,
+		timer12,
 		timer15,
+		timer16,
+		timer17,
 		timer23
 };
 
 
 #endif
 /************************************************
- *                     PWMService
+ *                     PWMservice
  ***********************************************/
 #ifdef HAL_TIM_MODULE_ENABLED
 
 map<Pin, PWMservice::Instance> PWMservice::available_instances = {
-		{PE14, PWMservice::Instance(&timer1, TIM_CHANNEL_4, NORMAL)}
+	{PB14, PWMservice::Instance(&timer12, TIM_CHANNEL_1, NORMAL)},
+	{PB15, PWMservice::Instance(&timer12, TIM_CHANNEL_2, NORMAL)},
+	{PB4, PWMservice::Instance(&timer3, TIM_CHANNEL_1, NORMAL)},
+	{PB5, PWMservice::Instance(&timer3, TIM_CHANNEL_2, NORMAL)},
+	{PC8, PWMservice::Instance(&timer3, TIM_CHANNEL_3, NORMAL)},
+	{PD12, PWMservice::Instance(&timer4, TIM_CHANNEL_1, NORMAL)},
+	{PD13, PWMservice::Instance(&timer4, TIM_CHANNEL_2, NORMAL)},
+	{PD15, PWMservice::Instance(&timer4, TIM_CHANNEL_4, NORMAL)},
+	{PE14, PWMservice::Instance(&timer1, TIM_CHANNEL_4, NORMAL)},
+	{PE6, PWMservice::Instance(&timer15, TIM_CHANNEL_2, NORMAL)},
+	{PF1, PWMservice::Instance(&timer23, TIM_CHANNEL_2, NORMAL)},
+	{PF2, PWMservice::Instance(&timer23, TIM_CHANNEL_3, NORMAL)},
+	{PF3, PWMservice::Instance(&timer23, TIM_CHANNEL_4, NORMAL)},
 };
 
 map<Pin, PWMservice::Instance> PWMservice::available_instances_negated = {};
 
 map<pair<Pin, Pin>, PWMservice::Instance> PWMservice::available_instances_dual = {
-		{{PE4, PE5}, PWMservice::Instance(&timer15, TIM_CHANNEL_1, DUAL)}
+	{{PB8,PB6}, PWMservice::Instance(&timer16, TIM_CHANNEL_1, DUAL)},
+	{{PB9,PB7}, PWMservice::Instance(&timer17, TIM_CHANNEL_1, DUAL)},
+	{{PE11,PE10}, PWMservice::Instance(&timer1, TIM_CHANNEL_2, DUAL)},
+	{{PE13,PE12}, PWMservice::Instance(&timer1, TIM_CHANNEL_3, DUAL)},
+	{{PE5,PE4}, PWMservice::Instance(&timer15, TIM_CHANNEL_1, DUAL)},
+	{{PE9,PE8}, PWMservice::Instance(&timer1, TIM_CHANNEL_1, DUAL)},
 };
 
 #endif
