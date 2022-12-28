a7f10d8ef46a15acc420ea06bd05240789cbfa44
alejandrogonzalvo <alejandrogonhid@protonmail.com>
2022-12-28 08:34:45 +0100

    manually change deprecated operators

--- Drivers/CMSIS/Device/ST/STM32H7xx/Include/stm32h7xx.h
+++ Drivers/CMSIS/Device/ST/STM32H7xx/Include/stm32h7xx.h
@@ -203,9 +203,9 @@ typedef enum
 /** @addtogroup Exported_macros
   * @{
   */
-#define SET_BIT(REG, BIT)     ((REG) |= (BIT))
+#define SET_BIT(REG, BIT)     ((REG) = (REG|BIT))
 
-#define CLEAR_BIT(REG, BIT)   ((REG) &= ~(BIT))
+#define CLEAR_BIT(REG, BIT)   ((REG) = (REG) & ~(BIT))
 
 #define READ_BIT(REG, BIT)    ((REG) & (BIT))
 
