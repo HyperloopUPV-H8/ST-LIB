3261d77de2f80ac02b60e5491f150e8d8a631259
Pablo <pablogscraft@gmail.com>
2023-01-14 09:35:49 +0100

    All working!

--- Inc/ST-LIB_LOW/ErrorHandler/ErrorHandler.hpp
+++ Inc/ST-LIB_LOW/ErrorHandler/ErrorHandler.hpp
@@ -2,7 +2,7 @@
  * ErrorHandler.hpp
  *
  *  Created on: Dec 22, 2022
- *      Author: predawnia
+ *      Author: Pablo
  */
 
 #pragma once
@@ -21,9 +21,24 @@ public:
 
 public:
 
-	template<Integral... Args>
+	template<class... Args>
 	static void ErrorHandler(string format, Args... args);
 
 	static void ErrorHandlerUpdate();
 
 };
+
+
+template<class... Args>
+void ErrorHandlerModel::ErrorHandler(string format, Args... args){
+	 ErrorHandlerModel::error_triggered = 1.0;
+
+	 const int32_t size = std::snprintf(nullptr, 0, format.c_str(), args...) + 1;
+	 const unique_ptr<char[]> buffer = std::make_unique<char[]>(size);
+
+	 std::snprintf(buffer.get(), size, format.c_str(), args...);
+
+	 description += std::string(buffer.get(), buffer.get() + size - 1);
+	 description += " | TimeStamp(nanoseconds): " + std::to_string(Time::get_global_tick());
+}
+
--- Src/ST-LIB_LOW/ErrorHandler/ErrorHandler.cpp
+++ Src/ST-LIB_LOW/ErrorHandler/ErrorHandler.cpp
@@ -2,28 +2,13 @@
  * ErrorHandler.cpp
  *
  *  Created on: Dec 22, 2022
- *      Author: predawnia
+ *      Author: Pablo
  */
 
 #include "ErrorHandler/ErrorHandler.hpp"
 
+string ErrorHandlerModel::description = "";
 double ErrorHandlerModel::error_triggered = 0;
-
-template<Integral... Args>
-void ErrorHandlerModel::ErrorHandler(string format, Args... args){
-	 ErrorHandlerModel::error_triggered = 1.0;
-
-	 if (!UART::printf_ready) {
-		return;
-	 }
-
-	 snprintf(description, 500 , format.c_str(), args...);
-
-
-	 description += " | TimeStamp(nanoseconds): ", Time::get_global_tick();
-
-}
-
 
 void ErrorHandlerModel::ErrorHandlerUpdate(){
 	if (!UART::printf_ready) {
@@ -33,7 +18,6 @@ void ErrorHandlerModel::ErrorHandlerUpdate(){
 	printf("Error: %s%s", ErrorHandlerModel::description.c_str(), endl);
 
 	//TODO:Enviar el mismo mensaje por ethernet.
-
 
 	//TODO: Parpadeo del led de fault.
 	//HAL_GPIO_TogglePin(GPIOx, GPIO_Pin);
