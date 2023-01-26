8a4330004b5b0a62d99ddfd54066a380ab5589ed
Stefan <stefanxenadu@gmail.com>
2023-01-26 09:54:58 +0100

    DigitalOutput Toggle

--- Src/HALAL/Services/DigitalOutputService/DigitalOutputService.cpp
+++ Src/HALAL/Services/DigitalOutputService/DigitalOutputService.cpp
@@ -6,6 +6,7 @@
  */
 
 #include "DigitalOutputService/DigitalOutputService.hpp"
+#include "ErrorHandler/ErrorHandler.hpp"
 
 uint8_t DigitalOutputService::id_counter = 0;
 map<uint8_t,Pin> DigitalOutputService::service_ids = {};
@@ -17,25 +18,42 @@ uint8_t DigitalOutputService::inscribe(Pin& pin){
 }
 
 void DigitalOutputService::turn_off(uint8_t id){
-	if (not DigitalOutputService::service_ids.contains(id))
+	if (not DigitalOutputService::service_ids.contains(id)){
+		ErrorHandler("ID %d is not registered as a DigitalOutput",id);
 		return;
+	}
 
 	Pin pin = DigitalOutputService::service_ids[id];
 	HAL_GPIO_WritePin(pin.port, pin.gpio_pin, (GPIO_PinState)PinState::OFF);
 }
 
 void DigitalOutputService::turn_on(uint8_t id){
-	if (not DigitalOutputService::service_ids.contains(id))
+	if (not DigitalOutputService::service_ids.contains(id)){
+		ErrorHandler("ID %d is not registered as a DigitalOutput",id);
 		return;
+	}
 
 	Pin pin = DigitalOutputService::service_ids[id];
 	HAL_GPIO_WritePin(pin.port, pin.gpio_pin, (GPIO_PinState)PinState::ON);
 }
 
 void DigitalOutputService::set_pin_state(uint8_t id, PinState state){
-	if (not DigitalOutputService::service_ids.contains(id))
+	if (not DigitalOutputService::service_ids.contains(id)){
+		ErrorHandler("ID %d is not registered as a DigitalOutput",id);
 		return;
+	}
 
 	Pin pin = DigitalOutputService::service_ids[id];
 	HAL_GPIO_WritePin(pin.port, pin.gpio_pin, (GPIO_PinState) state);
+}
+
+void DigitalOutputService::toggle(uint8_t id){
+	if (not DigitalOutputService::service_ids.contains(id)){
+		ErrorHandler("ID %d is not registered as a DigitalOutput",id);
+		return;
+	}
+
+	Pin pin = DigitalOutputService::service_ids[id];
+	GPIO_PinState actual_state = HAL_GPIO_ReadPin(pin.port,pin.gpio_pin);
+	HAL_GPIO_WritePin(pin.port, pin.gpio_pin, (GPIO_PinState)!actual_state);
 }
