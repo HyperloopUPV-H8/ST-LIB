34e00a7b153a46f2f01cf02ba7e1cc850e1e85cc
alejandrogonzalvo <alejandrogonhid@protonmail.com>
2023-01-27 18:16:47 +0100

    add init data

--- Inc/HALAL/Models/DMA/DMA.hpp
+++ Inc/HALAL/Models/DMA/DMA.hpp
@@ -11,9 +11,10 @@
 #include "stm32h7xx_hal.h"
 
 
+template <class HandleType>
 class DMA {
 public:
-	enum Stream : uint8_t {
+	enum NVICnumber : uint8_t {
 		DMA1Stream0 = 11,
 		DMA1Stream1 = 12,
 		DMA1Stream2 = 13,
@@ -31,6 +32,30 @@ public:
 		DMA2Stream7 = 70,
 	};
 
+	struct InitData {
+		DMA_Stream_TypeDef* instance;
+		NVICnumber nvic_number;
+		uint8_t adc_request;
+		uint8_t direction;
+		uint8_t mode;
+		HandleType linked_handle;
+		DMA_HandleTypeDef* handle_pointer;
+
+		InitData() = default;
+		InitData(DMA_Stream_TypeDef* instance, uint8_t adc_request, uint8_t direction, uint8_t mode, NVICnumber nvic_number=0) :
+			instance(instance), adc_request(adc_request), direction(direction), mode(mode), nvic_number(nvic_number) {};
+	};
+
+	class Stream {
+	public:
+		InitData init_data;
+		DMA_HandleTypeDef handle;
+
+		Stream() = default;
+		Stream(DMA_HandleTypeDef handle, InitData init_data);
+
+	};
+
 	static void inscribe_stream();
 	static void inscribe_stream(Stream dma_stream);
 	static void start();
@@ -39,3 +64,37 @@ private:
 	static vector<Stream> available_streams;
 	static vector<Stream> inscribed_streams;
 };
+
+template <class HandleType>
+void DMA::activate_IT() {
+	__HAL_RCC_DMA1_CLK_ENABLE();
+	__HAL_RCC_DMA2_CLK_ENABLE();
+
+	for (Stream dma_stream : inscribed_streams) {
+		NVICnumber& nvic_number = dma_stream.init_data.nvic_number;
+		if (nvic_number) {
+			HAL_NVIC_SetPriority((IRQn_Type)dma_stream, 0, 0);
+			HAL_NVIC_EnableIRQ((IRQn_Type)dma_stream);
+		}
+
+		DMA_HandleTypeDef& handle = dma_stream.handle;
+		InitData& i = dma_stream.init_data;
+	    handle.Instance = i.instance;
+	    handle.Init.Request = i.adc_request;
+	    handle.Init.Direction = i.direction;
+	    handle.Init.PeriphInc = DMA_PINC_DISABLE;
+	    handle.Init.MemInc = DMA_MINC_ENABLE;
+	    handle.Init.PeriphDataAlignment = DMA_PDATAALIGN_HALFWORD;
+	    handle.Init.MemDataAlignment = DMA_MDATAALIGN_HALFWORD;
+	    handle.Init.Mode = dma_stream,init_data.mode;
+	    handle.Init.Priority = DMA_PRIORITY_LOW;
+	    handle.Init.FIFOMode = DMA_FIFOMODE_DISABLE;
+	    if (HAL_DMA_Init(&handle) != HAL_OK)
+	    {
+	      Error_Handler();
+	    }
+
+	    __HAL_LINKDMA(
+	    		i.linked_handle, i.handle_pointer, handle);
+	}
+}
--- Src/HALAL/Models/DMA/DMA.cpp
+++ Src/HALAL/Models/DMA/DMA.cpp
@@ -7,30 +7,23 @@
 
 #include "DMA/DMA.hpp"
 
-vector<DMA::Stream> DMA::available_streams = {
-		DMA1Stream0, DMA1Stream1, DMA1Stream2, DMA1Stream3, DMA1Stream4, DMA1Stream5, DMA1Stream6, DMA2Stream0, DMA2Stream1, DMA2Stream2, DMA2Stream3, DMA2Stream4, DMA2Stream5, DMA2Stream6, DMA2Stream7,
-};
-
 vector<DMA::Stream> DMA::inscribed_streams = {};
 
 void DMA::inscribe_stream() {
 	if (available_streams.empty()) {
-		return; //TODO: error handler
+		ErrorHandler("There are not any DMA streams available");
+		return;
 	}
 	inscribed_streams.push_back(available_streams.back());
 	available_streams.pop_back();
 }
+
 void DMA::inscribe_stream(Stream dma_stream) {
 	if (std::find(available_streams.begin(), available_streams.end(), dma_stream) == available_streams.end()) {
-		return; //TODO: error handler
+		Error_Handler("The selected DMA stream isn't available");
+		return;
 	}
+
 	inscribed_streams.push_back(dma_stream);
 }
 
-void DMA::start() {
-	  __HAL_RCC_DMA1_CLK_ENABLE();
-	for (Stream dma_stream : inscribed_streams) {
-		HAL_NVIC_SetPriority( (IRQn_Type)dma_stream, 0, 0);
-		HAL_NVIC_EnableIRQ( (IRQn_Type)dma_stream);
-	}
-}
