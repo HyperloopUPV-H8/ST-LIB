0063881f0d9c982ebb48116426a953916577870d
Pablo <pablogscraft@gmail.com>
2023-01-31 20:44:46 +0100

    removed comment

--- Src/HALAL/Models/TimerPeripheral/TimerPeripheral.cpp
+++ Src/HALAL/Models/TimerPeripheral/TimerPeripheral.cpp
@@ -35,15 +35,6 @@ void TimerPeripheral::init() {
 		handle->Init.ClockDivision = TIM_CLOCKDIVISION_DIV1;
 		handle->Init.RepetitionCounter = 0;
 		handle->Init.AutoReloadPreload = TIM_AUTORELOAD_PRELOAD_DISABLE;
-//		if (HAL_TIM_Base_Init(handle) != HAL_OK)
-//		{
-//			ErrorHandler("Unable to init base clock on %d", name.c_str());
-//		}
-//		sClockSourceConfig.ClockSource = TIM_CLOCKSOURCE_INTERNAL;
-//		if (HAL_TIM_ConfigClockSource(handle, &sClockSourceConfig) != HAL_OK)
-//		{
-//			ErrorHandler("Unable to config clock source on %d", name.c_str());
-//		}
 
 		if (!init_data.input_capture_channels.empty()) {
 			if (HAL_TIM_Base_Init(handle) != HAL_OK)
