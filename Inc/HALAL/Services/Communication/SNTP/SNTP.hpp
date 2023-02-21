7a10d6d0686183058a13cbf45456f50fd4629ebe
ricardo chust <ricardochustfides@gmail.com>
2023-02-21 19:08:54 +0100

    created folder and files

---
+++ Inc/HALAL/Services/Communication/SNTP/SNTP.hpp
@@ -0,0 +1,10 @@
+/*
+ * SNTP.hpp
+ *
+ *  Created on: 21 feb. 2023
+ *      Author: Ricardo
+ */
+
+//https://community.st.com/s/question/0D53W00000j5I4aSAE/how-set-up-pretty-presice-and-detailed-rtc-on-stm32h743zi2
+
+//tontos usando sntp para configurar rtc
---
+++ Src/HALAL/Services/Communication/SNTP/SNTP.cpp
@@ -0,0 +1,6 @@
+/*
+ * SNTP.cpp
+ *
+ *  Created on: 21 feb. 2023
+ *      Author: Ricardo
+ */
