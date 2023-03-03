33bc9badca8bba0032b6839e86bb9b05a4485d78
ricardo chust <ricardochustfides@gmail.com>
2023-03-03 16:24:48 +0100

    SNTP works

--- Src/HALAL/Services/Communication/SNTP/SNTP.cpp
+++ Src/HALAL/Services/Communication/SNTP/SNTP.cpp
@@ -7,8 +7,10 @@
 
 #include "Communication/SNTP/SNTP.hpp"
 #include <lwip/ip_addr.h>
+#include "sntp_opts.h"
 #include "sntp.h"
 #include "IPV4/IPV4.hpp"
+#include "Time/Time.hpp"
 
 void SNTP::sntp_update(uint8_t address_head, uint8_t address_second, uint8_t address_third, uint8_t address_last){
 	sntp_setoperatingmode(SNTP_OPMODE_POLL);
@@ -16,4 +18,8 @@ void SNTP::sntp_update(uint8_t address_head, uint8_t address_second, uint8_t add
 	IP_ADDR4(address,address_head,address_second,address_third,address_last);
 	sntp_setserver(0,address);
 	sntp_init();
+}
+
+extern "C" void set_rtc(uint16_t counter, uint8_t second, uint8_t minute, uint8_t hour, uint8_t day, uint8_t month, uint16_t year){
+	Time::set_RTC_data(counter, second, minute, hour, day, month, year);
 }
