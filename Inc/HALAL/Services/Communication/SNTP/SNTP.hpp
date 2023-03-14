e29f3cea3accb2b69180fce0f0ff7f37dc544f3e
ricardo chust <ricardochustfides@gmail.com>
2023-03-14 16:05:41 +0100

    removed comment (upsn't)

--- Inc/HALAL/Services/Communication/SNTP/SNTP.hpp
+++ Inc/HALAL/Services/Communication/SNTP/SNTP.hpp
@@ -5,10 +5,6 @@
  *      Author: Ricardo
  */
 
-//https://community.st.com/s/question/0D53W00000j5I4aSAE/how-set-up-pretty-presice-and-detailed-rtc-on-stm32h743zi2
-
-//tontos usando sntp para configurar rtc
-
 #pragma once
 
 #include "C++Utilities/CppUtils.hpp"
