aa891b879bd4de930d7404f361579f7cafe86802
alejandrogonzalvo <alejandrogonhid@protonmail.com>
2023-04-26 17:59:27 +0200

    initial constructor

--- Inc/HALAL/Models/Packets/PacketValue.hpp
+++ Inc/HALAL/Models/Packets/PacketValue.hpp
@@ -119,3 +119,64 @@ public:
 template<class Type> requires Container<Type>
 PacketValue(Type)->PacketValue<Type>;
 #endif
+
+template<class Type,size_t N> requires  std::is_pointer<Type>::value
+class PacketValue<Type(&)[N]>: public PacketValue<> {
+public:
+    using value_type = Type;
+    Type(*src )[N] = nullptr;
+    PacketValue() = default;
+    PacketValue(Type(*src)[N]): src(src) {}
+    ~PacketValue() = default;
+    void* get_pointer() override {
+        return src;
+    }
+    size_t get_size() override {
+        return N*sizeof(Type);
+    }
+    void parse(void* data) override {
+        memcpy(src, data, get_size());
+    }
+    void copy_to(void* data) override {
+        memcpy(data, src, get_size());
+    }
+};
+
+#if __cpp_deduction_guides >= 201606
+template<class Type,size_t N> 
+PacketValue(Type(*)[N])->PacketValue<Type(&)[N]>;
+#endif
+
+
+template<class Type,size_t N> 
+class PacketValue<Type*(&)[N]>: public PacketValue<> {
+public:
+    using value_type = Type*;
+    Type*(*src )[N] = nullptr;
+    PacketValue() = default;
+    PacketValue(Type*(*src)[N]): src(src) {}
+    ~PacketValue() = default;
+    void* get_pointer() override {
+        return src;
+    }
+    size_t get_size() override {
+        return N*sizeof(Type);
+    }
+    void parse(void* data) override {
+        for(Type* i : *src) {
+            *i = *(Type*)data;
+            data += sizeof(Type);
+        }
+    }
+    void copy_to(void* data) override {
+        for(Type* i : *src) {
+            *(Type*)data = *i;
+            data += sizeof(Type);
+        }
+    }
+};
+
+#if __cpp_deduction_guides >= 201606 
+template<class Type,size_t N>
+PacketValue(Type*(*)[N])->PacketValue<Type*(&)[N]>;
+#endif
