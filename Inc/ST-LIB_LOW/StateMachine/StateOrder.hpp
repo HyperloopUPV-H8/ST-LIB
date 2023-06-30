496c7c0e040e765f88d6fb75a10260c731cb15be
Pablo <pablogscraft@gmail.com>
2023-06-30 19:14:22 +0200

    fixed stateorder, fixed sntp ;), added warn

--- Inc/ST-LIB_LOW/StateMachine/StateOrder.hpp
+++ Inc/ST-LIB_LOW/StateMachine/StateOrder.hpp
@@ -19,15 +19,21 @@ public:
 	}
 
 	static void add_state_orders(vector<uint16_t>& new_ids){
-		if(informer_socket == nullptr) ErrorHandler("Informer Socket has not been set");
+		if(informer_socket == nullptr) {
+			ErrorHandler("Informer Socket has not been set");
+			return;
+		}
 		add_state_orders_order.set_pointer(1, &new_ids);
 		state_orders_ids_size = new_ids.size();
 		informer_socket->send_order(add_state_orders_order);
 	}
 
 	static void remove_state_orders(vector<uint16_t>& old_ids){
-		if(informer_socket == nullptr) ErrorHandler("Informer Socket has not been set");
-		add_state_orders_order.set_pointer(1, &old_ids);
+		if(informer_socket == nullptr){
+			ErrorHandler("Informer Socket has not been set");
+			return;
+		}
+		remove_state_orders_order.set_pointer(1, &old_ids);
 		state_orders_ids_size = old_ids.size();
 		informer_socket->send_order(remove_state_orders_order);
 	}
