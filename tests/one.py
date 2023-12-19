6fbc64e95f5a96898740d94e65c8dde59cc87565
orangepi-hyperloop-upv <128622965+hyperloop-orangeman@users.noreply.github.com>
2023-12-19 11:33:50 +0100

    Create one.py

---
+++ tests/one.py
@@ -0,0 +1,7 @@
+from colorama import Fore
+from time import sleep
+print(Fore.GREEN + "running dummy test")
+sleep(2)
+
+print("###################################################")
+print("Test completed succesfully" + Fore.RESET)
