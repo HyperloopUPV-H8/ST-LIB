9589d8e3f13a6abf04c507e909a531ee8d0a7ced
ricardochust <ricardochustfides@gmail.com>
2024-04-24 17:16:03 +0200

    adc pointers

--- Inc/ST-LIB_HIGH/Control/Blocks/MovingAverage.hpp
+++ Inc/ST-LIB_HIGH/Control/Blocks/MovingAverage.hpp
@@ -49,3 +49,50 @@ class MovingAverage : public ControlBlock<double,double> {
         }
 };
 
+
+template<class InputType, class OutputType, size_t N>
+class MovingAverageBlock : public ControlBlock<InputType,OutputType> {
+    private:
+		InputType buffer[N] = {(InputType)0};
+        uint32_t first = 0, last = -1;
+        uint32_t counter = 0;
+        InputType accumulator = 0.0;
+
+    public:
+        MovingAverageBlock(): ControlBlock<InputType,OutputType>(){this->output_value = 0;}
+
+        void execute() override {
+        	if(counter < N) {
+                last++;
+        		buffer[last] = this->input_value;
+        		accumulator += this->input_value/N;
+        		counter++;
+        		return;
+        	}
+        	accumulator -= buffer[first] / N;
+            first = (first + 1) % N;
+            last = (last + 1) % N;
+            buffer[last] = this->input_value;
+            accumulator += buffer[last] / N;
+            this->output_value = (OutputType) accumulator;
+            return;
+        }
+
+        double compute(InputType input_v) {
+        	this->input(input_v);
+        	execute();
+        	return this->output_value;
+        }
+
+        void reset(){
+        	first = 0;
+        	last = -1;
+        	counter = 0;
+        	accumulator = 0;
+        	this->output_value = 0;
+        	for(InputType& e: buffer){
+				e = 0;
+        	}
+        }
+};
+
