4abdc1fc7f18016eda1999d8eb854b9c00d701e3
victhor <victorlopezcortes25@gmail.com>
2026-03-02 19:13:02 +0100

    more formatting fixes

--- Inc/MockedDrivers/compiler_specific.hpp
+++ Inc/MockedDrivers/compiler_specific.hpp
@@ -52,8 +52,8 @@ inline uint32_t __CLZ(uint32_t val) {
     _BitScanReverse(&idx, val);
     return 31 - idx;
 #else
-    for(uint32_t i = 0; i < 32; i++) {
-        if((val & (1 << (31 - i))) != 0)
+    for (uint32_t i = 0; i < 32; i++) {
+        if ((val & (1 << (31 - i))) != 0)
             return i;
     }
     return 32;
@@ -69,8 +69,8 @@ inline uint32_t __CTZ(uint32_t val)
     _BitScanForward(&idx, val);
     return idx;
 #else
-    for(uint32_t i = 0; i < 32; i++) {
-        if((val & (1 << i)) != 0)
+    for (uint32_t i = 0; i < 32; i++) {
+        if ((val & (1 << i)) != 0)
             return i;
     }
     return 32;
