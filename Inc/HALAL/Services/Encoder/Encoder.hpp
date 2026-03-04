93b4c5ad0c1a28ae62405a7be7fe8a167e858bec
Jorge Sáez <jorgeesg82@gmail.com>
2026-03-04 19:34:00 +0100

    hardened a lot the encoder, being sure that instances are properly set

--- Inc/HALAL/Services/Encoder/Encoder.hpp
+++ Inc/HALAL/Services/Encoder/Encoder.hpp
@@ -26,6 +26,38 @@ template <const TimerDomain::Timer& dev> class Encoder {
     inline static TimerWrapper<dev>* timer;
     inline static bool is_on = false;
 
+    static TimerWrapper<dev>* require_timer_wrapper(const char* error_message) {
+        if (timer == nullptr) {
+            ErrorHandler(error_message);
+            return nullptr;
+        }
+        return timer;
+    }
+
+    static TimerDomain::Instance* require_counter_instance(const char* error_message) {
+        TimerWrapper<dev>* timer_wrapper = require_timer_wrapper(error_message);
+        if (timer_wrapper == nullptr) {
+            return nullptr;
+        }
+        if (timer_wrapper->instance == nullptr || timer_wrapper->instance->tim == nullptr) {
+            ErrorHandler(error_message);
+            return nullptr;
+        }
+        return timer_wrapper->instance;
+    }
+
+    static TimerDomain::Instance* require_hal_instance(const char* error_message) {
+        TimerDomain::Instance* instance = require_counter_instance(error_message);
+        if (instance == nullptr) {
+            return nullptr;
+        }
+        if (instance->hal_tim == nullptr) {
+            ErrorHandler(error_message);
+            return nullptr;
+        }
+        return instance;
+    }
+
 public:
     Encoder(TimerWrapper<dev>* tim) {
         if (timer == nullptr) {
@@ -34,7 +66,12 @@ public:
     }
 
     static void init(TimerWrapper<dev>* tim) {
-        timer = tim;
+        if (tim == nullptr || tim->instance == nullptr || tim->instance->tim == nullptr ||
+            tim->instance->hal_tim == nullptr) {
+            ErrorHandler("Unable to init encoder, timer is null");
+            return;
+        }
+
         TIM_Encoder_InitTypeDef sConfig = {0};
         TIM_MasterConfigTypeDef sMasterConfig = {0};
 
@@ -50,6 +87,7 @@ public:
 
         if (HAL_TIM_Encoder_Init(tim->instance->hal_tim, &sConfig) != HAL_OK) {
             ErrorHandler("Unable to init encoder");
+            return;
         }
 
         sMasterConfig.MasterOutputTrigger = TIM_TRGO_RESET;
@@ -57,21 +95,28 @@ public:
         if (HAL_TIMEx_MasterConfigSynchronization(tim->instance->hal_tim, &sMasterConfig) !=
             HAL_OK) {
             ErrorHandler("Unable to config master synchronization in encoder");
+            return;
         }
 
         tim->instance->tim->PSC = 5;
         tim->instance->tim->ARR = 55000;
+        timer = tim;
     }
 
     static void turn_on() {
         if (is_on)
             return;
+        TimerDomain::Instance* instance =
+            require_hal_instance("Unable to start encoder, timer is not initialized");
+        if (instance == nullptr) {
+            return;
+        }
 
-        if (HAL_TIM_Encoder_GetState(timer->instance->hal_tim) == HAL_TIM_STATE_RESET) {
+        if (HAL_TIM_Encoder_GetState(instance->hal_tim) == HAL_TIM_STATE_RESET) {
             ErrorHandler("Unable to get state from encoder");
             return;
         }
-        if (HAL_TIM_Encoder_Start(timer->instance->hal_tim, TIM_CHANNEL_ALL) != HAL_OK) {
+        if (HAL_TIM_Encoder_Start(instance->hal_tim, TIM_CHANNEL_ALL) != HAL_OK) {
             ErrorHandler("Unable to start encoder");
             return;
         }
@@ -82,25 +127,65 @@ public:
     static void turn_off() {
         if (!is_on)
             return;
-        if (HAL_TIM_Encoder_Stop(timer->instance->hal_tim, TIM_CHANNEL_ALL) != HAL_OK) {
+        TimerDomain::Instance* instance =
+            require_hal_instance("Unable to stop encoder, timer is not initialized");
+        if (instance == nullptr) {
+            return;
+        }
+        if (HAL_TIM_Encoder_Stop(instance->hal_tim, TIM_CHANNEL_ALL) != HAL_OK) {
             ErrorHandler("Unable to stop encoder");
+            return;
         }
         is_on = false;
     }
 
-    static inline void reset() { timer->instance->tim->CNT = UINT32_MAX / 2; }
+    static inline void reset() {
+        TimerDomain::Instance* instance =
+            require_counter_instance("Unable to reset encoder, timer is not initialized");
+        if (instance == nullptr) {
+            return;
+        }
+        instance->tim->CNT = get_initial_counter_value();
+    }
 
-    static inline uint32_t get_counter() { return timer->instance->tim->CNT; }
+    static inline uint32_t get_counter() {
+        TimerDomain::Instance* instance =
+            require_counter_instance("Unable to read encoder counter, timer is not initialized");
+        if (instance == nullptr) {
+            return 0;
+        }
+        return instance->tim->CNT;
+    }
 
-    static inline bool get_direction() { return ((timer->instance->tim->CR1 & 0b10000) >> 4); }
+    static inline bool get_direction() {
+        TimerDomain::Instance* instance =
+            require_counter_instance("Unable to read encoder direction, timer is not initialized");
+        if (instance == nullptr) {
+            return false;
+        }
+        return ((instance->tim->CR1 & 0b10000) >> 4);
+    }
 
-    static inline uint32_t get_initial_counter_value() { return timer->instance->tim->ARR / 2; }
+    static inline uint32_t get_initial_counter_value() {
+        TimerDomain::Instance* instance = require_counter_instance(
+            "Unable to read encoder initial counter, timer is not initialized"
+        );
+        if (instance == nullptr) {
+            return 0;
+        }
+        return instance->tim->ARR / 2;
+    }
 
     static int64_t get_delta_clock(uint64_t clock_time, uint64_t last_clock_time) {
+        TimerWrapper<dev>* timer_wrapper =
+            require_timer_wrapper("Unable to compute encoder delta clock, timer is not initialized");
+        if (timer_wrapper == nullptr) {
+            return 0;
+        }
         int64_t delta_clock = clock_time - last_clock_time;
         if (clock_time < last_clock_time) { // overflow handle
             delta_clock = clock_time +
-                          CLOCK_MAX_VALUE * NANO_SECOND / timer->get_clock_frequency() -
+                          CLOCK_MAX_VALUE * NANO_SECOND / timer_wrapper->get_clock_frequency() -
                           last_clock_time;
         }
         return delta_clock;
