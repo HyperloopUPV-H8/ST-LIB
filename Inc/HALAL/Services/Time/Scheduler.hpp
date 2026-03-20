773c44602566054c184feb2795f1412920c01b5d
victhor <victorlopezcortes25@gmail.com>
2026-03-20 15:25:42 +0100

    get cleanup to compile

--- Inc/HALAL/Services/Time/Scheduler.hpp
+++ Inc/HALAL/Services/Time/Scheduler.hpp
@@ -33,7 +33,7 @@ struct Scheduler {
     // temporary, will be removed
     [[deprecated]] static inline void start() {}
     static void update();
-    static inline uint64_t get_global_tick();
+    static uint64_t get_global_tick();
 
     static uint16_t register_task(uint32_t period_us, callback_t func);
     static bool unregister_task(uint16_t id);
