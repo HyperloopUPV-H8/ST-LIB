6c2681300b0ede68f5c3237f0aad99af6c2a939c
victhor <victorlopezcortes25@gmail.com>
2026-04-01 12:53:42 +0200

    fix old compile error which shouldn't be one

--- Inc/HALAL/Models/TimerDomain/TimerDomain.hpp
+++ Inc/HALAL/Models/TimerDomain/TimerDomain.hpp
@@ -546,7 +546,8 @@ TimerXList
                     ST_LIB::compile_error("This timer is used by the scheduler");
                 }
 
-                if (requests[i].request != TimerRequest::AnyGeneralPurpose &&
+                if ((requests[i].request != TimerRequest::AnyGeneralPurpose) &&
+                    (requests[i].request != TimerRequest::Any32bit) &&
                     (requests[i].request < 1 || requests[i].request > 24 ||
                      (requests[i].request > 17 && requests[i].request < 23))) {
                     ST_LIB::compile_error("Invalid TimerRequest value for timer");
