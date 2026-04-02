a3e7392fb9c78ce197ac63226d9a6b294ea29c44
victhor <victorlopezcortes25@gmail.com>
2026-04-02 12:15:54 +0200

    fix indentation

--- Inc/HALAL/Models/TimerDomain/TimerDomain.hpp
+++ Inc/HALAL/Models/TimerDomain/TimerDomain.hpp
@@ -576,12 +576,12 @@ struct TimerDomain {
                 ST_LIB::compile_error("This timer is used by the scheduler");
             }
 
-                if ((requests[i].request != TimerRequest::AnyGeneralPurpose) &&
-                    (requests[i].request != TimerRequest::Any32bit) &&
-                    (requests[i].request < 1 || requests[i].request > 24 ||
-                     (requests[i].request > 17 && requests[i].request < 23))) {
-                    ST_LIB::compile_error("Invalid TimerRequest value for timer");
-                }
+            if ((requests[i].request != TimerRequest::AnyGeneralPurpose) &&
+                (requests[i].request != TimerRequest::Any32bit) &&
+                (requests[i].request < 1 || requests[i].request > 24 ||
+                    (requests[i].request > 17 && requests[i].request < 23))) {
+                ST_LIB::compile_error("Invalid TimerRequest value for timer");
+            }
 
             uint32_t used_channels = 0;
             for (uint16_t pi = 0; pi < requests[i].pin_count; pi++) {
