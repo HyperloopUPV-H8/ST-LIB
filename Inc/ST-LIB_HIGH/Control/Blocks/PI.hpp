47b56a2839dce24fe8facae619fd831fb02e4bb7
oganigl <osganig@gmail.com>
2026-04-22 13:47:56 +0200

    added a type to choose if float or doubles you want to use

--- Inc/ST-LIB_HIGH/Control/Blocks/PI.hpp
+++ Inc/ST-LIB_HIGH/Control/Blocks/PI.hpp
@@ -3,23 +3,25 @@
 #include "../ControlBlock.hpp"
 #include "Integrator.hpp"
 
-template <IntegratorType IntegratorMethod> class PI : public ControlBlock<double, double> {
+template <IntegratorType IntegratorMethod, typename T> class PI : public ControlBlock<T, T> {
+    static_assert(std::is_floating_point_v<T>, "T must be a floating point type");
+
 public:
-    double kp;
-    double error;
+    T kp;
+    T error;
     Integrator<IntegratorMethod> integrator;
 
 public:
     PI() = default;
-    PI(double kp, double ki, double period) : kp(kp), integrator(period, ki) {}
+    PI(T kp, T ki, T period) : kp(kp), integrator(period, ki) {}
     void execute() override {
         integrator.input(this->input_value);
         integrator.execute();
         error = this->input_value;
         this->output_value = kp * error + integrator.output_value;
     }
-    void set_kp(double kp) { this->kp = kp; }
-    void set_ki(double ki) {
+    void set_kp(T kp) { this->kp = kp; }
+    void set_ki(T ki) {
         integrator.ki = ki;
         integrator.reset();
     }
