099a32b045dab224db4340cc1066fae939d040f2
victhor <victorlopezcortes25@gmail.com>
2026-04-23 12:43:28 +0200

    TimerWrapper: Add helper to set the callback+data

--- Inc/HALAL/Services/Time/TimerWrapper.hpp
+++ Inc/HALAL/Services/Time/TimerWrapper.hpp
@@ -423,6 +423,11 @@ template <const TimerDomain::Timer& dev> struct TimerWrapper {
     // TODO: 16 bit and 32 bit version (?)
     inline void set_limit_value(uint32_t arr) { instance->tim->ARR = arr; }
 
+    inline void set_callback(void (*callback)(void*), void *callback_data) {
+        TimerDomain::callbacks[instance->timer_idx] = callback;
+        TimerDomain::callback_data[instance->timer_idx] = callback_data;
+    }
+
     inline void configure32bit(void (*callback)(void*), void* callback_data, uint32_t period) {
         static_assert(
             this->is_32bit_instance,
