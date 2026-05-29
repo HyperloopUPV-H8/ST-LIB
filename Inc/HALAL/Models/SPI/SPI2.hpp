56bcf897b1defae01965cbdef45f31fbad51a56f
FoniksFox <borisbeslimov@gmail.com>
2026-05-29 18:06:46 +0200

    fix(SPI): Fix clock

--- Inc/HALAL/Models/SPI/SPI2.hpp
+++ Inc/HALAL/Models/SPI/SPI2.hpp
@@ -1363,7 +1363,7 @@ struct SPIDomain {
                     spi_number = 2;
                 } else if (peripheral == SPIPeripheral::spi3) {
                     PeriphClkInitStruct.PeriphClockSelection = RCC_PERIPHCLK_SPI3;
-                    PeriphClkInitStruct.Spi123ClockSelection = RCC_SPI123CLKSOURCE_PLL;
+                PeriphClkInitStruct.Spi123ClockSelection = RCC_SPI123CLKSOURCE_PLL;
                     if (HAL_RCCEx_PeriphCLKConfig(&PeriphClkInitStruct) != HAL_OK) {
                         PANIC("Unable to configure SPI3 clock");
                     }
@@ -1371,7 +1371,7 @@ struct SPIDomain {
                     spi_number = 3;
                 } else if (peripheral == SPIPeripheral::spi4) {
                     PeriphClkInitStruct.PeriphClockSelection = RCC_PERIPHCLK_SPI4;
-                    PeriphClkInitStruct.Spi45ClockSelection = RCC_SPI45CLKSOURCE_PLL2;
+                    PeriphClkInitStruct.Spi45ClockSelection = RCC_SPI45CLKSOURCE_PCLK2;
                     if (HAL_RCCEx_PeriphCLKConfig(&PeriphClkInitStruct) != HAL_OK) {
                         PANIC("Unable to configure SPI4 clock");
                     }
@@ -1379,7 +1379,7 @@ struct SPIDomain {
                     spi_number = 4;
                 } else if (peripheral == SPIPeripheral::spi5) {
                     PeriphClkInitStruct.PeriphClockSelection = RCC_PERIPHCLK_SPI5;
-                    PeriphClkInitStruct.Spi45ClockSelection = RCC_SPI45CLKSOURCE_PLL2;
+                    PeriphClkInitStruct.Spi45ClockSelection = RCC_SPI45CLKSOURCE_PCLK2;
                     if (HAL_RCCEx_PeriphCLKConfig(&PeriphClkInitStruct) != HAL_OK) {
                         PANIC("Unable to configure SPI5 clock");
                     }
