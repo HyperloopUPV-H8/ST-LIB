b1df6511045e4280794d698ab9e54dd552cfcce6
Jorge Sáez <jorgeesg82@gmail.com>
2026-06-02 20:35:20 +0200

    fix SPI45 2MHz clock source

--- Src/HALAL/Models/HALconfig/Halconfig.cpp
+++ Src/HALAL/Models/HALconfig/Halconfig.cpp
@@ -19,7 +19,10 @@ void HALconfig::system_clock() {
 
     __HAL_RCC_PLL_PLLSOURCE_CONFIG(RCC_PLLSOURCE_HSE);
 
-    RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_LSI | RCC_OSCILLATORTYPE_HSE;
+    RCC_OscInitStruct.OscillatorType =
+        RCC_OSCILLATORTYPE_HSI | RCC_OSCILLATORTYPE_LSI | RCC_OSCILLATORTYPE_HSE;
+    RCC_OscInitStruct.HSIState = RCC_HSI_DIV1;
+    RCC_OscInitStruct.HSICalibrationValue = RCC_HSICALIBRATION_DEFAULT;
     RCC_OscInitStruct.LSIState = RCC_LSI_ON;
     RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
     RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSE;
