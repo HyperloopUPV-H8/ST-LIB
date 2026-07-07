c4519f3828f9196406be395bef6abb5042c0dc2f
FoniksFox <borisbeslimov@gmail.com>
2026-07-07 17:03:45 +0200

    fix(Scheduler): Make get_global_tick() monotonic (add an if)

--- Src/HALAL/Services/Time/Scheduler.cpp
+++ Src/HALAL/Services/Time/Scheduler.cpp
@@ -101,10 +101,15 @@ void Scheduler::update() {
 }
 
 uint64_t Scheduler::get_global_tick() {
+    static uint64_t last_tick{0};
     SchedLock();
     uint64_t tick = global_tick_us_;
     tick += Scheduler_global_timer->CNT;
     SchedUnlock();
+    if (tick < last_tick) [[unlikely]] {
+        tick = last_tick;
+    }
+    last_tick = tick;
     return tick;
 }
 
